2bdc3e35dd3a009652cce9bb2c2ab269df222418
visage <itlw@foxmail.com>
2017-08-07 14:37:26 +0800

    add

---
+++ work/AutoFuDongMenu/js/main.js
@@ -0,0 +1,62 @@
+﻿// JavaScript source code
+$(document).ready(function () {
+    //获取三个div
+    var $divs = $(".main div");
+    //获取所有选项卡
+    var $dLis = $(".dItem");
+
+    //浏览器窗口可视区宽高
+    var $wWidth, $wHeight;
+
+    function changeWH() {
+        //获取浏览器可视区宽高
+        $wWidth = $(window).width();
+        $wHeight = $(window).height();
+        $divs.each(function () {
+            $(this).width($wWidth);
+            $(this).height($wHeight);
+        });
+    }
+
+    //页面加载完成后调用一次
+    changeWH();
+    //当浏览器窗口大小发生变化时，触发的事件
+    $(window).resize(changeWH);
+
+    //初始化
+    funScroll();
+
+
+    //浏览器滚动条滚动触发的事件
+    $(window).scroll(funScroll);
+
+    //条滚动事件方法
+    function funScroll () {
+        //获取当前滚动条的高度
+        var top = $(document).scrollTop();
+
+        //遍历所有的div
+        $divs.each(function (index) {
+            var $divObj = $(this);
+            var thisTop = $divObj.offset().top;
+            if (top >= thisTop) {
+                //获取当前高亮的选项
+                var $activeObj = $dLis.find(".active");
+                if ($dLis[index] && $($dLis[index]) != $activeObj) {
+                    $($dLis[index]).addClass("active").siblings().removeClass("active");
+                }
+            }
+        });
+    }
+
+
+    //选项卡点击事件
+    $dLis.click(function () {
+        //更改选中项和其他兄弟节点的样式
+        $(this).addClass("active").siblings().removeClass("active");
+        var index = $dLis.index(this);
+        //平滑到对应的div位置
+        $("html,body").animate({ scrollTop: $($divs[index]).offset().top }, 300);
+
+    })
+})
---
+++ work/weiXinChat/js/index.js
@@ -0,0 +1,95 @@
+﻿//定义页面变量
+var btn = $('#btnSend');   //获取【发送按钮】
+        
+//发送消息方法
+btn.click(function () {
+    var text = $('#sendText').val().trim();   //获取【编辑文字框】
+    if (text == '') {
+        alert('不能发送空消息');
+    } else {
+        send(text, 'Text'); //0文本消息					
+    }
+});
+
+//发送消息事件
+function send(msg, type) {
+    var t = $("#sendText").text();
+    var n = 0;
+    msgShow(t, n)
+}
+
+
+//消息处理（显示）t：html
+function msgShow(t, n) {
+    try {
+        if (isNullOrEmpty(t)) {
+            alert("发送内容不能为空！");
+            return false;
+        }
+
+        var txt = '';
+        if (n == 0) {
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            var day = date.getDate();
+            var hour = date.getHours();
+            var minute = date.getMinutes();
+            var second = date.getSeconds();
+            var dateTime = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;
+            var nowheadimg = 'images/2.png';
+            
+                txt += '  <div class="meChat">' +
+                          '    <div class="dateTime">' + dateTime + '</div>' +
+                          '    <div class="bubble clearfix">' +
+                          '        <img class="fr" src="' + nowheadimg + '" alt="Alternate Text" />' +
+                          '        <div class="chatContext fr clearfix">' +
+                          '            <div class="chatText fr">' + t + ' </div>' +
+                          '        </div>' +
+                          '    </div>' +
+                          '</div>';
+        } 
+        $(".talkWindow").append(txt);
+        $(".talkWindow").scrollTop(99999);
+    }
+    catch (e) { alert(e) }
+}
+
+
+//给聊天气泡中的图片添加点击事件
+$(".bubble .chatContext img").click(function () {
+    var img = new Image();
+    img.src = $(this).attr('src');
+    var imgWidth = img.width; //图片实际宽度 
+    $(".mask").find("img").attr('src', img.src);
+    if(imgWidth<$(this).parent().width()){
+        $(".mask").find("img").css({ "width": "atuo", "height": "none", "position": "relative", "margin": "auto" });
+    }else{
+        $(".mask").find("img").css({ "width": "atuo", "height": "auto", "position": "absolute", "margin": "inherit" });
+    }
+
+    $(".mask").show();
+});
+
+
+        
+
+
+//设置遮罩层的高度为窗口高度
+$(".mask").css({ 'height': $(window).height() + 'px', "overflow": "auto" });
+//遮罩层关闭按钮的事件
+$(".mask .closeBtn").click(function () {
+    $(".mask").hide();
+})
+
+
+//验证字符串是否为空
+function isNullOrEmpty(strVal) {
+    if (strVal == '' || strVal == null || strVal == undefined) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
